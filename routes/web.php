8a8ad686b0d7694a19ad6579b5625f965f2dd8d7
Dionísio <dionisioni304@gmail.com>
2025-03-23 06:22:26 +0100

    Metendo todas as rotas dadashboard protegido por uma meddleware

--- routes/web.php
+++ routes/web.php
@@ -4,47 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
 Route::get('/', function () {
-    return view('project.index');
+    return view('project.landingPage');
 });
 
-
-Route::get('/quartos', [ProdutoController::class, 'quarto']);
-
-Route::get('/hospedes', [ProdutoController::class, 'hospedes']);
-
-Route::get('/Check-in', [ProdutoController::class, 'checkin']);
-
-
-Route::get('/faturas', function () {
-    return view('project.fatura');
-});
-
-Route::get('/', function () {
-    return view('project.index');
-});
-
-Route::get('/ver', function () {
-    return view('project.ver');
-});
-
-Route::get('/add-quarto', function () {
-    return view('project.add-quarto');
-});
-
-
-
-Route::middleware(['admin'])->group(function(){
-    Route::get('/admin', function () {
-        return dd('administrado');
-    });
-});
-
-
-Route::middleware(['maneger'])->group(function(){
-    Route::get('/maneger', function () {
-        return dd('gerente');
-    });
-});
+Route::get('/reserve-online', [ProdutoController::class, 'reserva_on']);
 
 
 Route::middleware([
@@ -52,7 +15,39 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    
+    Route::get('/index', [ProdutoController::class, 'index']);
+
+    Route::get('/quartos', [ProdutoController::class, 'quarto']);
+
+    Route::get('/hospedes', [ProdutoController::class, 'hospedes']);
+
+    Route::get('/Check-in', [ProdutoController::class, 'checkin']);
+
+    Route::get('/faturas', [ProdutoController::class, 'fatura']);
+
+    Route::get('/add-quarto', [ProdutoController::class, 'add_quarto']);
+
+    Route::get('/ver', function () {
+        return view('project.ver');
+    });
+    
     Route::get('/dashboard', function () {
         return view('project.dashboard');
     })->name('dashboard');
 });
+
+
+/**
+Route::middleware(['admin'])->group(function(){
+    Route::get('/admin', function () {
+        return dd('administrado');
+    });
+});
+
+Route::middleware(['maneger'])->group(function(){
+    Route::get('/maneger', function () {
+        return dd('gerente');
+    });
+});
+**/
