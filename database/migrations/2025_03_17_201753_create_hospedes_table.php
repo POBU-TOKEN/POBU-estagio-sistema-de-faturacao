8a9c5bb1634408f4bee97f4df1ee24d36c3817bb
Dionísio <dionisioni304@gmail.com>
2025-03-23 06:29:16 +0100

    adicionando a migrations de hospedes

--- database/migrations/2025_03_17_201753_create_hospedes_table.php
+++ database/migrations/2025_03_17_201753_create_hospedes_table.php
@@ -13,6 +13,23 @@ return new class extends Migration
     {
         Schema::create('hospedes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('number_of_contribuethor');
+            $table->string('address');
+            $table->string('email');
+            $table->string('country');
+            $table->string('was_born');
+            $table->date('check_out_date');
+            $table->text('description');
+            $table->boolean('sex');
+            $table->string('document');
+            $table->string('host_type');
+            $table->string('people');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+             ->references('id')
+             ->on('users');
             $table->timestamps();
         });
     }
