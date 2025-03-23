dceccdf53a3b55bf762a7eeafdae07ec6a850ca3
Dionísio <dionisioni304@gmail.com>
2025-03-23 06:26:23 +0100

    adicionando modelo de conexao com o bd dos hospedes

--- app/Models/Hospede.php
+++ app/Models/Hospede.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Hospede extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id'];
+
+    public function user(){
+        return $this->belongsTo(user::class);
+    }
 }
