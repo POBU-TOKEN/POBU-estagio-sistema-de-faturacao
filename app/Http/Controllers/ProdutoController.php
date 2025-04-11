7f6b993f844b02d41c4d8005e2b02c272ff65551
Dionísio <dionisioni304@gmail.com>
2025-04-11 12:00:12 +0100

    Adicionando logica de reserva online, validacoes e etc...

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -67,7 +67,46 @@ class ProdutoController extends Controller{
         return view('project.reserva-on');
     }
 
+    
+
     public function storage_reserva_on(Request $req){
+
+        $req->validate([
+            'name' => 'required|string|max:255',
+            'number' => 'required|numeric',
+            'cNumber' => 'required|numeric',
+            'address' => 'required',
+            'emailAddress' => 'required|email|max:255',
+            'country' => 'required|string',
+            'sex' => 'required',
+            'description' => 'required',
+            'ocupantes' => 'required|numeric|max:8',
+            'hostType' => 'required|string|max:255',
+            'checkOut' => 'required|date',
+            'born' => 'required|date',
+        ],
+        [
+            'name.required' => 'O nome é necessário!',
+            'name.string' => 'Tende ser caracteres não númericos',
+            'name.max' => 'Valor máximo é :max',
+            'number.required' => 'O número de telefone é necessário',
+            'number.numeric' => 'Valor deve ser numerico',
+            'cNumber.required' => 'O número de contribuente é necessário',
+            'cNumber.numeric' => 'Valor deve ser numerico',
+            'address.required' => 'O indereço é obrigatório',
+            'address.max' => 'Valor máximo é :max',
+            'emailAddress.required' => 'O campo email é necessário',
+            'emailAddress.max' => 'Valor máximo é :max',
+            'country.required' => 'O campo país é necessário',
+            'country.string' => 'O não valor deve ser numeríco',
+            'sex.required' => 'O campo sexo é necessário',
+            'description.required' => 'A descrição é obrigatória',
+            'ocupantes.required' => 'O número de ocupantes é necessário',
+            'hostType.required' => 'O tipo de hospede é necessário',
+            'checkOut.required' => 'Data de checkout é necessário',
+            'born.required' => 'Data de nascimento é necessário',
+            ]);
+
         $res = new Reserva_on();
         $res->name = $req->name;
         $res->phone_number = $req->number;
@@ -75,8 +114,8 @@ class ProdutoController extends Controller{
         $res->address = $req->address;
         $res->email = $req->emailAddress;
         $res->country = $req->country;
-        // $res->was_born = $req->born;
-        // $res->check_out_date = $req->checkOut;
+        $res->was_born = $req->born;
+        $res->check_out_date = $req->checkOut;
         $res->description = $req->description;
         $res->sex = $req->sex;
         $res->document = $req->document;
@@ -85,10 +124,9 @@ class ProdutoController extends Controller{
 
 
         if($res->save()){
-            dd("sucesso");
-            redirect('/')->with('msg', 'A sua reserva foi salva com sucesso, deseja salvar PDF?');
+            return redirect('/')->with('msg', 'A sua reserva foi salva com sucesso, dirija-se antes ou a hora marcada, deseja salvar PDF?');
         }else{
-            redirect('/')->with('erro', 'Algo deu errado, tente mais tarde ou actualize seu browser e tente novamente.');
+            return redirect('/')->with('erro', 'Algo deu errado, tente mais tarde ou actualize seu browser e tente novamente.');
         }
     }
 }
